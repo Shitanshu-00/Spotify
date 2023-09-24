d1d35da35d7aa14ae03d6a10e2b0bf47b80886a7
Shitanshu Tripathi <tripathis1106@gmail.com>
2023-09-24 15:01:47 +0530

    Navigation Added

---
+++ src/Player.js
@@ -0,0 +1,14 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import { styles } from '../Components/styles';
+
+const Player = () => {
+    console.log("Player");
+  return (
+    <View style={styles.container}>
+      <Text>Player</Text>
+    </View>
+  )
+}
+
+export default Player
